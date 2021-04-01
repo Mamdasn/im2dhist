aa89b3f45c6bbf5e4b925e5699db136e64fb8f04
mamdasn <mamdassn@gmail.comclear>
2021-04-01 19:21:29 +0430

    minor updates

--- setup.py
+++ setup.py
@@ -10,17 +10,18 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name='im2dhist',
-    version='0.0.1',
+    version='0.0.2.2',
     author="mamdasn s",
     author_email="<mamdassn@gmail.com>",
     url="https://github.com/Mamdasn/im2dhist",
     description='This small piece of code is intended to help researchers, especially those in field of image processing, to easily calculate two dimensional histogram of a given image.',
     long_description=long_description,
     long_description_content_type = "text/markdown",
-    packages=find_packages(),
+    package_dir={'': 'src'},
+    py_modules=["im2dhist"],
     install_requires=[
-        "numpy ~= 1.18.4", 
-        "tqdm ~= 4.51.0",
+        "numpy", 
+        "tqdm",
         ],
     keywords=['python', 'histogram', 'imhist', '2dhist', 'hist2d'],
     classifiers=[
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from src.im2dhist import im2dhist
