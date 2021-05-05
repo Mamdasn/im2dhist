2ff09293a8d04a93e4d60a2507e75aeee74b8608
mamdasn <mhsaber1375@gmail.com>
2021-05-05 13:38:54 +0430

    Update repo

--- src/im2dhist.py
+++ src/im2dhist.py
@@ -3,7 +3,7 @@ import numba
 
 @numba.njit()
 def imhist(arr):
-    hist = np.zeros((256), dtype=np.int16)
+    hist = np.zeros((256), dtype=np.uint32)
     for item in arr.copy().reshape(-1):
         hist[item] += 1 
     return hist
@@ -32,26 +32,23 @@ def im2dhist(image, w_neighboring = 6):
             xi_n[:, ii] = xi + (ii - w_neighboring)
             yi_n[:, ii] = yi + (ii - w_neighboring)
     
-        for row_idx, xi_n_row in enumerate(xi_n):
-            for column_idx, item in enumerate(xi_n_row):
-                if item >= h:
-                    xi_n[row_idx, column_idx] = -1
-        for row_idx, yi_n_row in enumerate(yi_n):
-            for column_idx, item in enumerate(yi_n_row):
-                if item >= w:
-                    yi_n[row_idx, column_idx] = -1
+        
+        xi_n = np.where(xi_n<h, xi_n, -1*np.ones_like(xi_n))
+        yi_n = np.where(yi_n<w, yi_n, -1*np.ones_like(yi_n))
         
         
         for i_row in range(xi_n.shape[0]):
-            xi_nr = xi_n[i_row, :] 
-            yi_nr = yi_n[i_row, :] 
-            xi_nr = xi_nr[np.where(xi_nr>=0)] 
-            yi_nr = yi_nr[np.where(yi_nr>=0)] 
+            xi_nr = xi_n[i_row, :].copy() 
+            yi_nr = yi_n[i_row, :].copy() 
+            xi_nr = xi_nr[np.where(xi_nr>=0)].copy()
+            yi_nr = yi_nr[np.where(yi_nr>=0)].copy()
             
             neighboring_intens = V[xi_nr[0]:xi_nr[-1]+1, yi_nr[0]:yi_nr[-1]+1].copy().reshape(-1).astype(np.int16)
             
             for neighboring_inten in neighboring_intens:
-                Hist2D[i, X_inv[neighboring_inten]-1] += np.abs(neighboring_inten+1-X[i]) +1
+                if i < X_inv[neighboring_inten]:
+                    Hist2D[i, X_inv[neighboring_inten]-1] += np.abs(neighboring_inten+1-X[i]) +1
+    Hist2D = Hist2D + np.tril(Hist2D.T, -1)
+    print(Hist2D)
     Hist2D_normalized = Hist2D/np.sum(Hist2D)
-    # return Hist2D_normalized
     return Hist2D_normalized
