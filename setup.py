0e1afd665ce2a22b8d42bd42042a41c57bd384dc
Mamdasan S <mhsaber1375@gmail.com>
2023-12-23 15:46:51 +0100

    Increment version for a major update + rewrite

--- setup.py
+++ setup.py
@@ -2,34 +2,34 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+with codecs.open(os.path.join(parent_dir, "README.md"), encoding="utf-8") as readme:
+    readme_description = "\n" + readme.read()
 
 
 setup(
     name='im2dhist',
-    version='0.1.5',
+    version='1.0.0',
     author="mamdasn s",
     author_email="<mamdassn@gmail.com>",
     url="https://github.com/Mamdasn/im2dhist",
     description='Calculates two dimensional histogram of a given image.',
-    long_description=long_description,
+    long_description=readme_description,
     long_description_content_type = "text/markdown",
     include_package_data=True,
     package_dir={'': 'src'},
     py_modules=["im2dhist"],
     install_requires=[
-        "numpy", 
+        "numpy",
         "numba",
         ],
     keywords=['python', 'histogram', 'imhist', '2dhist', 'hist2d', 'two dimensional histogram'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
-    ]
+    ],
 )
