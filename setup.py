b48e8b242fb22d1273a69338dcccb4811d5b2927
Mamdasan Sabrian <mhsaber1375@gmail.com>
2024-02-17 17:38:55 +0100

    Get version from __init__ file directly
    + Functionize the `readme_description` code snippet

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
 from setuptools import setup, find_packages
-import codecs
 import os
+
 
 parent_dir = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(parent_dir, "README.md"), encoding="utf-8") as readme:
-    readme_description = "\n" + readme.read()
+def get_version():
+    with open(os.path.join(parent_dir, "src", "__init__.py")) as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return line.strip().split("=")[1].strip(" '\"")
+
+def get_readme():
+    with open(os.path.join(parent_dir, "README.md"), encoding="utf-8") as readme:
+        readme_description = "\n" + readme.read()
+        return readme_description
 
 
 setup(
     name="im2dhist",
-    version="1.0.0",
-    author="mamdasn s",
-    author_email="<mamdassn@gmail.com>",
+    version=get_version(),
+    author="Mamdasan Sabrian",
+    author_email="<reach.s.farhad@gmail.com>",
     url="https://github.com/Mamdasn/im2dhist",
     description="Calculates two dimensional histogram of a given image.",
-    long_description=readme_description,
+    long_description=get_readme(),
     long_description_content_type="text/markdown",
     include_package_data=True,
     package_dir={"": "src"},
+    packages=find_packages(where='src'),
     py_modules=["im2dhist"],
     install_requires=[
         "numpy",
